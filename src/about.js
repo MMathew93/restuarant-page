ebb999203e7ec1e161dac4ed3eb3fe3057c9d7dc
lyndexer <mckinney.m93@gmail.com>
2020-05-09 17:49:23 -0700

    having issues with eventListeners

--- src/about.js
+++ src/about.js
@@ -1,44 +1,52 @@
-import { loadFooter, navButtons } from './initialpage.js'
+import {loadFooter, navButtons} from './initialpage.js'
 
+//ABOUT LOAD
+const loadAbout = function () {
+    const content = document.getElementById('content')
+    content.innerHTML = ''
 //Non-Homepage Header
-const loadAbout = (function () {
-    const content= document.getElementById('content')
-    const contentWrapper= document.createElement('div')
-    contentWrapper.id = "content-wrapper"
-    const header= document.createElement('header')
-    header.id= 'nonHomepageHeader'
-    const ul= document.createElement('ul')
-    ul.className= "nav-bar"
-    ul.id= "non-homepage"
-    let logo= document.createElement('div')
-    logo.id= 'logo'
-    logo.innerHTML= 'the Mock Bar'
-    header.appendChild(logo)
-    let ul_tabs= ['About', 'Events', 'Menu', 'Contact Us']
-    ul_tabs.forEach((x)=> {
-        let li= document.createElement('li')
-        li.id=`${x}`
-        li.innerHTML= `${x}`
-        ul.appendChild(li)
-    });
-    header.appendChild(ul)
-    contentWrapper.appendChild(header)
-    content.appendChild(contentWrapper)
-//Hero Content Change
-    const hero= document.createElement('div')
-    hero.className= "hero"
-    hero.id= "aboutHero"
-    const h1= document.createElement('h1')
-    h1.innerHTML= "the Mock Bar"
-    hero.appendChild(h1)
-    const p= document.createElement('p')
-    p.innerHTML= 'Both a made up and fictaticious place, Mathew has fooled not only you must the mass gathering of people who are outside of his home insiting he pour them a cold brew. However while a great man of mystery and honor mathew has no intentions following through on such demands. For years he has thought of starting his own brewery or even a pub but alas he has now dedicated his time and efforts in trying to learn to prgram, he figured with more money he could always buy his own brewery later on in life, guess we\'ll see how this pans out for our hero.'
-    hero.appendChild(p)
-    contentWrapper.appendChild(hero)
-    content.appendChild(contentWrapper)
-
+    const newHeader = function () {
+        const contentWrapper = document.createElement('div')
+        contentWrapper.id = "content-wrapper"
+        const header = document.createElement('header')
+        header.id = 'nonHomepageHeader'
+        const ul = document.createElement('ul')
+        ul.className = "nav-bar"
+        ul.id = "non-homepage"
+        let logo = document.createElement('div')
+        logo.id = 'logo'
+        logo.innerHTML = 'the Mock Bar'
+        header.appendChild(logo)
+        let ul_tabs = ['About', 'Events', 'Menu', 'Contact Us']
+        ul_tabs.forEach((x) => {
+            let li = document.createElement('li')
+            li.id = `${x}`
+            li.innerHTML = `${x}`
+            ul.appendChild(li)
+        });
+        header.appendChild(ul)
+        contentWrapper.appendChild(header)
+        content.appendChild(contentWrapper)
+    }
+    newHeader()
+    //Hero Content Change
+    const aboutSection = function () {
+        const aboutSection = document.createElement('div')
+        aboutSection.className = "aboutSection"
+        const h1 = document.createElement('h1')
+        h1.innerHTML = "the Mock Bar"
+        aboutSection.appendChild(h1)
+        const p = document.createElement('p')
+        p.innerHTML = 'Both a made up and fictaticious place, Mathew has fooled not only you must the mass gathering of people who are outside of his home insiting he pour them a cold brew. However while a great man of mystery and honor mathew has no intentions following through on such demands. For years he has thought of starting his own brewery or even a pub but alas he has now dedicated his time and efforts in trying to learn to prgram, he figured with more money he could always buy his own brewery later on in life, guess we\'ll see how this pans out for our hero.'
+        aboutSection.appendChild(p)
+        contentWrapper.appendChild(aboutSection)
+        content.appendChild(contentWrapper)
+    }
+    aboutSection()
     loadFooter()
     navButtons()
-})();
+};
 
-export { loadAbout }
+
+
+export {loadAbout}
--- src/initialpage.js
+++ src/initialpage.js
@@ -1,64 +1,64 @@
-import { loadAbout } from './about.js'
+import {loadAbout} from './about.js'
 
 //Header Loader
-const loadHomePageHeader = (function () {
-    const content= document.getElementById('content')
-    const contentWrapper= document.createElement('div')
+const loadHomePageHeader = function () {
+    const content = document.getElementById('content')
+    const contentWrapper = document.createElement('div')
     contentWrapper.id = "content-wrapper"
-    const header= document.createElement('header')
-    const ul= document.createElement('ul')
-    ul.className= "nav-bar"
-    let ul_tabs= ['About', 'Events', 'Menu', 'Contact Us']
-    ul_tabs.forEach((x)=> {
-        let li= document.createElement('li')
-        li.id=`${x}`
-        li.innerHTML= `${x}`
+    const header = document.createElement('header')
+    const ul = document.createElement('ul')
+    ul.className = "nav-bar"
+    let ul_tabs = ['About', 'Events', 'Menu', 'Contact Us']
+    ul_tabs.forEach((x) => {
+        let li = document.createElement('li')
+        li.id = `${x}`
+        li.innerHTML = `${x}`
         ul.appendChild(li)
     });
     header.appendChild(ul)
     contentWrapper.appendChild(header)
     content.appendChild(contentWrapper)
-})();
+};
 //Hero Loader
-const loadHero = (function () {
-    const content= document.getElementById('content')
-    const contentWrapper= document.getElementById('content-wrapper')
-    const hero= document.createElement('div')
-    hero.className= "hero"
+const loadHero = function () {
+    const content = document.getElementById('content')
+    const contentWrapper = document.getElementById('content-wrapper')
+    const hero = document.createElement('div')
+    hero.className = "hero"
     contentWrapper.appendChild(hero)
     content.appendChild(contentWrapper)
-})();
+};
 //Footer Loader
-const loadFooter = (function () {
-    const content= document.getElementById('content')
-    const contentWrapper= document.getElementById('content-wrapper')
-    const footer= document.createElement('footer')
-    const iconBox= document.createElement('div')
-    iconBox.id= "icon-box"
-    let icons= ['fab fa-facebook-square', 'fab fa-instagram', 'fab fa-twitter-square']
-    icons.forEach((x)=> {
+const loadFooter = function () {
+    const content = document.getElementById('content')
+    const contentWrapper = document.getElementById('content-wrapper')
+    const footer = document.createElement('footer')
+    const iconBox = document.createElement('div')
+    iconBox.id = "icon-box"
+    let icons = ['fab fa-facebook-square', 'fab fa-instagram', 'fab fa-twitter-square']
+    icons.forEach((x) => {
         let i = document.createElement('i')
-        i.className= `${x}`
+        i.className = `${x}`
         iconBox.appendChild(i)
     });
     footer.appendChild(iconBox)
-    const p= document.createElement('p')
-    p.className= "footer-text"
-    p.innerHTML= 'Icons supplied by FA Free, <a href="https://fontawesome.com/license"> fontawesome.com/license </a>, changes to icon color only, no other changes applied.'
+    const p = document.createElement('p')
+    p.className = "footer-text"
+    p.innerHTML = 'Icons supplied by FA Free, <a href="https://fontawesome.com/license"> fontawesome.com/license </a>, changes to icon color only, no other changes applied.'
     footer.appendChild(p)
     contentWrapper.appendChild(footer)
     content.appendChild(contentwrapper)
-})();
+};
 //Navigation Buttons
-const navButtons= (function () {
-    const about_button= getElementById('About')
-    const events_button= getElementById('Events')
-    const menu_button= getElementById('Menu')
-    const contactUs_button= getElementById('Contact Us')
+const navButtons = function () {
+    let about_button = document.getElementById('About')
+    let events_button = document.getElementById('Events')
+    let menu_button = document.getElementById('Menu')
+    let contactUs_button = document.getElementById('Contact Us')
     about_button.addEventListener('click', loadAbout)
-    events_button.addEventListener('click', events)
-    menu_button.addEventListener('click', menu)
-    contactUs_button.addEventListener('click', contactUs)
-})();
+    //events_button.addEventListener('click', events)
+    //menu_button.addEventListener('click', menu)
+    //contactUs_button.addEventListener('click', contactUs)
+};
 
-export { loadHomePageHeader, loadHero, loadFooter, navButtons }
+export {loadHomePageHeader, loadHero, loadFooter, navButtons}
